a723eceedaeb129e8d2e94c977942be7ca9a7d45
Domains18 <gibsonsgibson88@gmail.com>
2023-01-16 10:00:21 +0300

    redux

--- src/redux/features/service/userService.js
+++ src/redux/features/service/userService.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const getUsers = async () => {
     const config = {
         headers: {
-            'x-apikey': "63be7360969f06502871ad7f"
+            'x-apikey': " // your secret key"
         }
     }
     const getData = await axios.get("https://touchinspiration-0ada.restdb.io/rest/sample", config)
@@ -15,7 +15,7 @@ const getUsers = async () => {
 const patchata = async (id, data) => {
     const response = await axios.patch(`https://touchinspiration-0ada.restdb.io/rest/sample/${id}`, data, {
         headers: {
-            'x-apikey': "63be7360969f06502871ad7f"
+            'x-apikey': " // your secret key"
         }
     })
     return response.data
