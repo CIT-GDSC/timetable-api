4b0022e395896f2a02754641a2860f68b92af8dc
Domains18 <gibsonsgibson88@gmail.com>
2023-05-19 14:24:24 +0300

    file cleanup

--- src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-// import dataReducer from "./features/dataSlice";
-import userSlice from "./features/service/userSlice";
-
-export default configureStore({
-  reducer:{
-    user: userSlice
-  }
-})
