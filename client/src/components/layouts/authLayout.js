e8cd4363f18649109c5aa5ed0d33c6c63a36e77f
Domains18 <gibsonsgibson88@gmail.com>
2023-05-21 13:10:08 +0300

    layouts
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

---
+++ client/src/components/layouts/authLayout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+import React from 'react'
+
+const authLayout = () => {
+  return (
+      <div className="Auth">
+        <Outlet />
+    </div>
+  )
+}
+
+export default authLayout
