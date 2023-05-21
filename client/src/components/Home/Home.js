1782bd10282b440074bea935d2c2d751f2b72fd6
Domains18 <gibsonsgibson88@gmail.com>
2023-05-21 23:38:15 +0300

    navbar
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Nav from '../Nav/Nav'
 import './Home.scss'
 
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='Home'>
+      <Nav />
+      <div className='Home__content'>
+        <h1 className='Home__content__title'>Welcome to the MERN Boilerplate</h1>
+        </div>
+    </div>
   )
 }
 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Nav.scss'
+import { Link } from 'react-router-dom'
+import { AiOutlineUser } from 'react-icons/ai'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='navbar'>
+      <div className="logo">
+        <h1>Time-Table API</h1>
+      </div>
+      <div className="auth">
+        <Link className='link'><AiOutlineUser className='icon'/>Login As A teacher?</Link>
+      </div>
+      </nav>
   )
 }
 
