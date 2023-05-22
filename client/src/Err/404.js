02ab9d5929540ff233e6ae9cce01f9c2612414d0
Domains18 <gibsonsgibson88@gmail.com>
2023-05-22 09:05:50 +0300

    css
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

---
+++ client/src/Err/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+      <div>
+          <h1>404. NOT FOUND</h1>
+          <p>Congratulations, You found Our error Page</p>
+          Lin
+    </div>
+  )
+}
+
+export default Error;
