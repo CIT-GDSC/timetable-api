2735e42b327fed6759b7be4c5c4dc24448301228
Domains18 <gibsonsgibson88@gmail.com>
2023-06-14 02:37:54 +0300

    API restructure usiing nest-like syntaxes
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- server/routes/public/timeTableRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-const {getAllTimeTableEntries, } = require('../../controllers/timeTable');
-
-
-
-router.get('/all', getAllTimeTableEntries);
