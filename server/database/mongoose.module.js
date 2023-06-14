ba80c15c09d3185396bf5a879cddd1737b3c14ae
Domains18 <gibsonsgibson88@gmail.com>
2023-06-14 03:21:44 +0300

    configured server with mongodb
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- server/database/mongoose.module.js
+++ server/database/mongoose.module.js
@@ -1,1 +1,17 @@
 
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
+
+
+const connectDatabase = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DB_URI);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+
+module.exports = { connectDatabase }
---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const colors = require('colors');
+const bodyParser = require('body-parser');
+const { mongoose } = require('mongoose');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const { errorhandler } = require('./middleware/errorHandler.module');
+const { connectDatabase } = require('./database/mongoose.module');
+
+
+
+// Connect to database
+connectDatabase();
+app.use(errorhandler);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`.yellow.bold);
+    console.log(`attempting to connect to database...`.yellow.bold);
+});
+
+mongoose.connection.once('open', () => {
+    console.log(`Successfully pinged connection with Mongodb at ${mongoose.connection.host}:${mongoose.connection.port}`.green.bold);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Error connecting to Mongodb: ${err.message}`.red.bold);
+});
+
