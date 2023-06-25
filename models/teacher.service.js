a1f8b01d81358c1af54948fdc0b4ed1824997e2c
Domains18 <gibsonsgibson88@gmail.com>
2023-06-25 13:03:49 +0300

    added group and teacher service schemas
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

---
+++ models/teacher.service.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+
+
+
+
+const teacherSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    teacherId: {
+        type: String,
+        required: true,
+    },
+    department: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: 'trainer',
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+
+const Teacher = mongoose.model('Teacher', teacherSchema);
+module.exports = Teacher;
