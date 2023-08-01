2d04453f746f1168be4d5e9468a6084de397bda5
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-08-02 00:04:08 +0300

    updated the schemas

--- models/goupsSchema.js
+++ models/goupsSchema.js
@@ -3,27 +3,11 @@ const mongoose = require('mongoose');
 
 
 const groupSchema = new mongoose.Schema({
-    groupName: {
-        type: String,
-        required: [true, 'Provide a unique username'],
-        unique: [true, "Please provide username"],
-    },
-    groupCourse: {
-        type: String,
-        required: String,
-    },
-    groupDepartment: {
-        type: String,
-        required: true,
-    },
-    groupMembers: {
-        type: Array,
-        required: true,
-    },
-    groupLeader: {
-        type: String,
-        required: true,
-    },
+    groupName: { type: String, required: true, unique: true, },
+    groupCourse: { type: String, required: String, },
+    groupDepartment: { type: String, required: true, },
+    groupMembers: { type: Array, required: true, }, 
+    groupLeader: { type: String, required: true, },
 });
 
 
--- models/teacherSchema.js
+++ models/teacherSchema.js
@@ -5,35 +5,14 @@ const mongoose = require('mongoose');
 
 
 const teacherSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    teacherId: {
-        type: String,
-        required: true,
-    },
-    department: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: 'trainer',
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-})
+    firstName: { type: String, required: true, },
+    lastName: { type: String, required: true, },
+    teacherId: { type: String, required: true, },
+    department: { type: String, required: true, },
+    email: { type: String, required: true, },
+    role: { type: String, default: 'trainer', },
+    password: { type: String, required: true, },
+});
 
 
 const Teacher = mongoose.model('Teacher', teacherSchema);
