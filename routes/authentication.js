7221d4ada88b131bf0eb833310fc84fa1a81917f
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-08-15 23:14:54 +0300

    admin authentication

--- routes/authentication.js
+++ routes/authentication.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const {register, login } = require('../controllers/student');
 
+
+
 module.exports = (router)=>{
     router.post('/auth/register',register);
     router.post('/auth/login', login);
